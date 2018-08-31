2df0c24ead22a054e30b57de814729d1d4e46f44
Ben <39468299+bhc15@users.noreply.github.com>
2018-08-31 11:24:19 -0700

    Add files via upload

--- Lesson3/main.js
+++ Lesson3/main.js
@@ -1,40 +1,42 @@
 var coords = [34.852619, -82.394012]; // coordinates of my hometown
 
+// load Map, MapView, Point, SimpleMarkerSymbol, and Graphic modules
 require([
-      "esri/Map",
-      "esri/views/MapView",
-      "esri/geometry/Point",
-      "esri/symbols/SimpleMarkerSymbol",
-      "esri/Graphic",
-      "dojo/domReady!"
+    "esri/Map",
+    "esri/views/MapView",
+    "esri/geometry/Point",
+	"esri/symbols/SimpleMarkerSymbol",
+	"esri/Graphic",
+	"dojo/domReady!"
     ], function(Map, MapView, Point, SimpleMarkerSymbol, Graphic) {
 
-      var myMap = new Map({
+	// create Map instance
+    var myMap = new Map({ 
         basemap: "streets"
       });
-
-      var myView = new MapView({
+	// create MapView instance
+    var myView = new MapView({ 
         container: "viewDiv",
         map: myMap,
         zoom: 5,
         center: [coords[1], coords[0]] // longitude, latitude
       });
-      
-      var pt = new Point({
+    // create Point instance  
+    var pt = new Point({ 
             latitude: coords[0],
             longitude: coords[1] 
       });
-      
-      var sym = new SimpleMarkerSymbol({
+    // create SimpleMarkerSymbol instance  
+    var sym = new SimpleMarkerSymbol({ 
             color: "blue",
             style: "square",
             size: 12
       });
-      
-      var ptGraphic = new Graphic({
+    // create Graphic instance  
+    var ptGraphic = new Graphic({ 
             geometry: pt,
             symbol: sym
       });
-      
-      myView.graphics.add(ptGraphic);
+    // add Graphic object to MapView  
+    myView.graphics.add(ptGraphic); 
     });
